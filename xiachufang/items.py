414a3bdc57341ad5b29949fc3324b081ccae0320
lxyazi <lxyazi_1994@126.com>
2018-03-26 14:41:55 +0800

    2018.3.26 添加新的item 中途保存

--- xiachufang/items.py
+++ xiachufang/items.py
@@ -8,14 +8,19 @@
 import scrapy
 
 
-
+# 每道菜的信息
 class XiachufangItem(scrapy.Item):
     title = scrapy.Field()
     ings = scrapy.Field()
     # TODO:图片资源
 
-
+# 总分类信息
 class XiachufangTpyeItem(scrapy.Item):
     cates_list_info = scrapy.Field()
     cates_list = scrapy.Field()
     cates_list_herf = scrapy.Field()
+
+# 子分类信息
+class TpyeInfoItem(scrapy.Item):
+    # TODO
+    pass
--- xiachufang/spiders/xiachufang_spider.py
+++ xiachufang/spiders/xiachufang_spider.py
@@ -5,73 +5,86 @@ from xiachufang.items import XiachufangTpyeItem
 
 
 class XiachufangtypeSpiderSpider(scrapy.Spider):
-	name = 'xiachufang_spider'
-	allowed_domains = ['xiachufang.com']
-	start_urls = ['https://www.xiachufang.com/category/']
+    name = 'xiachufang_spider'
+    allowed_domains = ['xiachufang.com']
+    start_urls = ['https://www.xiachufang.com/category/']
 
-	def parse(self, response):
-		count = 0
-		node_list_1 = response.xpath(".//div[@class='cates-list clearfix has-bottom-border pb20 mb20']")
-		for node1 in node_list_1:
-			cates_list_info = node1.xpath("./div[@class='cates-list-info ml15 float-left']/h3/text()").extract()[0]
-			h4_cates_list = []
-			h4_cates_list_herf = []
-			node_list_h4 = node1.xpath(".//div[@class='cates-list-all clearfix hidden']/h4")
+    def parse(self, response):
+        count = 0
+        node_list_1 = response.xpath(".//div[@class='cates-list clearfix has-bottom-border pb20 mb20']")
+        for node1 in node_list_1:
+            cates_list_info = node1.xpath("./div[@class='cates-list-info ml15 float-left']/h3/text()").extract()[0]
+            h4_cates_list = []
+            h4_cates_list_herf = []
+            node_list_h4 = node1.xpath(".//div[@class='cates-list-all clearfix hidden']/h4")
 
-			print("------------test1------------------")
-			print(len(node_list_h4))
-			print("------------test2------------------")
+            print("------------test1------------------")
+            print(len(node_list_h4))
+            print("------------test2------------------")
 
-			for node_h4 in node_list_h4:
+            for node_h4 in node_list_h4:
 
-				if len(node_h4.xpath("./a")) != 0:
-					h4_cates_list.append(node_h4.xpath("./a/text()").extract()[0])
-					h4_cates_list_herf.append(node_h4.xpath("./a/@href").extract()[0])
-				else:
-					h4_cates_list.append(" ")
-					h4_cates_list_herf.append(" ")
+                if len(node_h4.xpath("./a")) != 0:
+                    h4_cates_list.append(node_h4.xpath("./a/text()").extract()[0])
+                    h4_cates_list_herf.append(node_h4.xpath("./a/@href").extract()[0])
+                else:
+                    h4_cates_list.append(" ")
+                    h4_cates_list_herf.append(" ")
 
-			node_list_ul = node1.xpath(".//div[@class='cates-list-all clearfix hidden']/ul")
-			i = 0
-			for node_ul in node_list_ul:
-				if len(node_ul.xpath("./li")) == 0:
-					item = XiachufangTpyeItem()
-					item['cates_list_info'] = cates_list_info
-					item['cates_list'] = h4_cates_list[i]
-					item['cates_list_herf'] = "https://www.xiachufang.com" + h4_cates_list_herf[i]
-					yield item
-					yield scrapy.Request(item['cates_list_herf'], meta={'cates_list_info': item['cates_list_info']},
-					                     callback=self.listParse)
-					# count += 1
+            node_list_ul = node1.xpath(".//div[@class='cates-list-all clearfix hidden']/ul")
+            i = 0
+            for node_ul in node_list_ul:
 
-					print("------------test1------------------")
-					print(item['cates_list_info'])
-					print(item['cates_list'])
-					print(item['cates_list_herf'])
-					print("------------test2------------------")
+                # ul下的目录item
+                if len(node_ul.xpath("./li")) == 0:
+                    item = XiachufangTpyeItem()
+                    item['cates_list_info'] = cates_list_info
+                    item['cates_list'] = h4_cates_list[i]
+                    item['cates_list_herf'] = "https://www.xiachufang.com" + h4_cates_list_herf[i]
+                    yield item
+                    yield scrapy.Request(item['cates_list_herf'], callback=self.listParse)
 
-				else:
-					for node_li in node_ul.xpath("./li"):
-						item = XiachufangTpyeItem()
-						item['cates_list_info'] = cates_list_info
-						item['cates_list'] = node_li.xpath("./a/text()").extract()[0]
-						item['cates_list_herf'] = "https://www.xiachufang.com" + node_li.xpath("./a/@href").extract()[0]
-						yield item
-						yield scrapy.Request(item['cates_list_herf'], meta={'cates_list_info': item['cates_list_info']},
-						                     callback=self.listParse)
-						# count += 1
+                    # yield scrapy.Request(item['cates_list_herf'], meta={'cates_list_info': item['cates_list_info']},
+                    #                      callback=self.listParse)
+                    # count += 1
 
-						print("------------test1------------------")
-						print(item['cates_list_info'])
-						print(item['cates_list'])
-						print(item['cates_list_herf'])
-						print("------------test2------------------")
+                    # 输出结果，进行检验
+                    print("------------test1------------------")
+                    print(item['cates_list_info'])
+                    print(item['cates_list'])
+                    print(item['cates_list_herf'])
+                    print("------------test2------------------")
 
-	def listParse(self, response):
-		# TODO
-		messageFromParse = response.meta
-		pass
+                # 若ul下无目录，则使用h4作为目录item
+                else:
+                    for node_li in node_ul.xpath("./li"):
+                        item = XiachufangTpyeItem()
+                        item['cates_list_info'] = cates_list_info
+                        item['cates_list'] = node_li.xpath("./a/text()").extract()[0]
+                        item['cates_list_herf'] = "https://www.xiachufang.com" + node_li.xpath("./a/@href").extract()[0]
+                        yield item
+                        yield scrapy.Request(item['cates_list_herf'], callback=self.listParse)
 
-	def itemParse(self, response):
-		# TODO
-		pass
+                        # yield scrapy.Request(item['cates_list_herf'], meta={'cates_list_info': item['cates_list_info']},
+                        #                      callback=self.listParsee)
+                        # count += 1
+
+                        # 输出结果，进行检验
+                        print("------------test1------------------")
+                        print(item['cates_list_info'])
+                        print(item['cates_list'])
+                        print(item['cates_list_herf'])
+                        print("------------test2------------------")
+
+    def listParse(self, response):
+        # TODO
+        # 提取分类标题
+        title = response.xpath(".//div[@class='pure-g' or @class='pure-u align-middle']//h1/text()").extract()[0]
+
+
+        pass
+
+
+    def itemParse(self, response):
+        # TODO
+        pass
